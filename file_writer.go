fb42696b1ddadedbe6938a1ef02e07ca97fe4458
kockockockoc <koc@kocmuk.ru>
2017-03-14 16:43:33 +0300

    execute renewLease on every NewBlock added

--- file_writer.go
+++ file_writer.go
@@ -222,6 +222,23 @@ func (f *FileWriter) startNewBlock() error {
 		previous = f.block.GetB()
 	}
 
+	// execute renewLease on every NewBlock (dirty hack)
+	// https://github.com/colinmarc/hdfs/issues/60
+	renewLeaseReq := &hdfs.RenewLeaseRequestProto{
+                ClientName: proto.String(f.client.namenode.ClientName()),
+        }
+        renewLeaseResp := &hdfs.RenewLeaseResponseProto{}
+
+        err := f.client.namenode.Execute("renewLease", renewLeaseReq, renewLeaseResp)
+        if err != nil {
+                if nnErr, ok := err.(*rpc.NamenodeError); ok {
+                        err = interpretException(nnErr.Exception, err)
+                }
+
+                return &os.PathError{"create", f.name, err}
+        }
+	// 
+	
 	addBlockReq := &hdfs.AddBlockRequestProto{
 		Src:        proto.String(f.name),
 		ClientName: proto.String(f.client.namenode.ClientName()),
